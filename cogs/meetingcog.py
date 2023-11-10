a50ad9e058b8903ce9085c891aecb8690684113d
Jmcgee01125 <jmcgee01125@gmail.com>
2023-11-10 14:13:53 -0500

    meeting cog worky

--- cogs/meetingcog.py
+++ cogs/meetingcog.py
@@ -1,3 +1,5 @@
+from helpers.buttonviews import ConfirmButtons, ButtonEnums
+from helpers.embedhelper import build_embed, EmbedField
 from nextcord import Interaction, SlashOption
 from typing import Union, Dict, List
 from nextcord.ext import commands
@@ -50,10 +52,10 @@ class TimeBlock:
         self.start = start
         self.end = end
 
-    def overlap(self, other: "TimeBlock") -> bool:
+    def overlaps(self, other: "TimeBlock") -> bool:
         if not isinstance(other, TimeBlock):
             return False
-        return self.start > other.end or self.end < other.start
+        return self.start <= other.end or self.end >= other.start
 
     def _get_ts_str(self) -> str:
         cur = datetime.now()
@@ -78,27 +80,59 @@ class MeetingUser:
         self.timeblocks: List[TimeBlock] = []
 
 
+class Meeting:
+    def __init__(self, timeblock: TimeBlock):
+        self.timeblock = timeblock
+        self.users: List[MeetingUser] = []
+        self.valid = True
+
+    def __str__(self) -> str:
+        return f"{self.timeblock}"
+
+    def register_user(self, user: MeetingUser) -> bool:
+        if not self.valid:
+            return False
+        if user in self.users:
+            return True
+        for tb in user.timeblocks:
+            if self.timeblock.overlaps(tb):
+                return False
+        self.users.append(user)
+        user.timeblocks.append(self.timeblock)
+        return True
+
+    def deregister_user(self, user: MeetingUser) -> bool:
+        if not self.valid:
+            return False
+        for u in self.users:
+            if u is user:
+                self.users.remove(u)
+                user.timeblocks.remove(self.timeblock)
+                return True
+        return False
+
+    def teardown(self):
+        for u in self.users:
+            self.deregister_user(u)
+        self.valid = False
+
+
 class MeetingCog(commands.Cog):
     """
     Meeting command cog
 
     Commands
     --------
-    meeting (+4)
+    meeting (+5)
     """
 
     def __init__(self, client: nextcord.Client):
-        # TODO: make a Meeting object to track meeting info more than just blocks
-        #        should support two buttons on the meeting: RSVP yes and no
-        #        if RSVPing yes, check that they aren't overlapping with any busy times
-        #        in either case, add them to a field of yes and no that is shown in the embed
-        #        (also, make the embed)
         # TODO: users should be able to un-busy themselves
         #       also, we'd need a way to keep track of who is in what meeting - reference meetings in MeetingUser?
         #       remember to update that information if meetings are canceled
         # TODO: users should be able to view the meetings they have RSVP'd to
         self.client: nextcord.Client = client
-        self.meetings: Dict[int, TimeBlock] = {}
+        self.meetings: Dict[int, Meeting] = {}
         self.next_meeting_id = 1
         self.users: Dict[nextcord.Member, MeetingUser] = {}
 
@@ -121,13 +155,14 @@ class MeetingCog(commands.Cog):
         if st >= en:
             await interaction.send("Start time must come before end time.", ephemeral=True)
             return
-        self.meetings[self.next_meeting_id] = TimeBlock(st, en)
+        self.meetings[self.next_meeting_id] = Meeting(TimeBlock(st, en))
         self.next_meeting_id += 1
         await interaction.send(f"Added meeting {self.next_meeting_id - 1}: {self.meetings[self.next_meeting_id-1]}")
 
     @_meeting.subcommand(name="cancel", description="Cancel a meeting")
     async def _mt_cancel(self, interaction: Interaction, id: int):
         try:
+            self.meetings[id].teardown()
             del self.meetings[id]
         except KeyError:
             await interaction.send("No such meeting.", ephemeral=True)
@@ -154,6 +189,24 @@ class MeetingCog(commands.Cog):
     @_meeting.subcommand(name="view", description="View info about a meeting")
     async def _mt_view(self, interaction: Interaction, id: int):
         try:
-            await interaction.send(f"Meeting {id} is planned for {self.meetings[id]}")
+            await self._manage_meeting_embed(interaction, id, self.meetings[id])
         except KeyError:
             await interaction.send("No such meeting.", ephemeral=True)
+
+    async def _manage_meeting_embed(self, interaction: Interaction, id: int, meeting: Meeting):
+        button_timeout = 60
+        buttons = ConfirmButtons(timeout=button_timeout)
+        fields = [EmbedField(title=f"{e.user.display_name}", content="", inline=True) for e in meeting.users]
+        emb = build_embed(*fields, title=f"Meeting {id}: {meeting}")
+        await interaction.send(embed=emb, view=buttons)
+        while not buttons.is_finished():
+            await buttons.wait()
+            if buttons.value == ButtonEnums.ACCEPT:
+                meeting.register_user(self._get_user(buttons.user))
+            elif buttons.value == ButtonEnums.DENY:
+                meeting.deregister_user(self._get_user(buttons.user))
+            fields = [EmbedField(title=f"{e.user}", content="", inline=True) for e in meeting.users]
+            emb = build_embed(*fields, title=f"Meeting {id}: {meeting}")
+            buttons = ConfirmButtons(timeout=button_timeout)
+            await interaction.edit_original_message(embed=emb, view=buttons)
+        await interaction.edit_original_message(embed=emb, view=None)
